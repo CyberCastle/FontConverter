b694aa8556573c076014ad4ed8a50662e9de3b8c
CyberCastle <cybercastle@gmail.com>
2019-11-16 20:15:13 -0300

    Fix return type

--- test/test.js
+++ test/test.js
@@ -12,15 +12,15 @@ function cb(code, errorCode, errorMessage) {
 
 const cbPtr = wasm.addFunction(cb, 'viii')
 const func = wasm.cwrap('convert', 'string', ['string', 'number', 'number', 'number', 'number'])
-const func_cb = wasm.cwrap('convertcb', 'string', ['string', 'number', 'number', 'number', 'number', 'pointer'])
+const func_cb = wasm.cwrap('convertcb', null, ['string', 'number', 'number', 'number', 'number', 'pointer'])
 
 wasm.onRuntimeInitialized = function() {
     console.log('JSON Output:')
     console.log(func('monaco.ttf', 5, 1, 50, 55))
 
     console.log('C++ Output:')
-    console.log(func_cb('RobotoMono-Regular.ttf', 5, 0, null, null, cbPtr))
+    func_cb('RobotoMono-Regular.ttf', 5, 0, null, null, cbPtr)
 
     console.log('C++ Output with error:')
-    console.log(func_cb('font-doesnt-exist.ttf', 5, 0, null, null, cbPtr))
+    func_cb('font-doesnt-exist.ttf', 5, 0, null, null, cbPtr)
 }
